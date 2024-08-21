379464cd10bd8888373fa63bad91ad4cd19fd4ca
Anagha-lin <linusanagha37@gmail.com>
2024-08-21 22:49:55 +0000

    a

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,45 +1,29 @@
 #!/usr/bin/python3
+"""
+UTF-8 Validation
+"""
+
+
 def validUTF8(data):
     """
-    Determines if a given data set represents a valid UTF-8 encoding.
-    
-    :param data: List of integers, where each integer represents a byte (8 bits)
-    :return: True if data is a valid UTF-8 encoding, otherwise False
+    Data: list of integers
+    Return: True if data is a valid UTF-8
+    encoding, else return False
     """
-    # Number of bytes remaining in the current UTF-8 character
-    bytes_to_process = 0
+    byte_count = 0
 
-    # Masks to identify byte patterns
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
-
-    for byte in data:
-        # Extract the 8 least significant bits of the byte
-        byte = byte & 0xFF
-
-        if bytes_to_process == 0:
-            # Count how many 1's are in the leading part of the byte
-            mask = 1 << 7
-            while mask & byte:
-                bytes_to_process += 1
-                mask = mask >> 1
-
-            # If no 1's are found, it's a single-byte character
-            if bytes_to_process == 0:
-                continue
-
-            # UTF-8 characters can only be 2 to 4 bytes long
-            if bytes_to_process == 1 or bytes_to_process > 4:
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
                 return False
-
         else:
-            # Check if the byte starts with '10'
-            if not (byte & mask1 and not (byte & mask2)):
+            if i >> 6 != 0b10:
                 return False
-
-        # Decrement the number of bytes left to process
-        bytes_to_process -= 1
-
-    # If all bytes were correctly processed, bytes_to_process should be 0
-    return bytes_to_process == 0
-
+            byte_count -= 1
+    return byte_count == 0
